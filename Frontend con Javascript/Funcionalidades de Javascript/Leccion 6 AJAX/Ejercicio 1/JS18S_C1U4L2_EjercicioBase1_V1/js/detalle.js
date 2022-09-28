df60a5934d28b6c7662d2ba55d4b6f0fbc9c8240
jorgee1 <johndoe@example.com>
2022-09-27 19:33:46 -0500

    Ejercicio 1 Leccion 6 Terminado

--- Frontend con Javascript/Funcionalidades de Javascript/Leccion 6 AJAX/Ejercicio 1/JS18S_C1U4L2_EjercicioBase1_V1/js/detalle.js
+++ Frontend con Javascript/Funcionalidades de Javascript/Leccion 6 AJAX/Ejercicio 1/JS18S_C1U4L2_EjercicioBase1_V1/js/detalle.js
@@ -3,27 +3,25 @@
 $(document).ready(function () {
 
   //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
-  var id = window.location.href.match(/id=(\d)*/)[1]
-  var data
+  try {
+    var id = location.href.match(/id=(\d)*/)[1]
+  } catch (error) {
+    console.log("No se incluyo el id");
+  }
+  var data, evento, html
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX y Guarda el resultado en una variable
   $.ajax({
     url: "http://127.0.0.1:5500/info.json"
-  }).done(function(value) {
+  }).done(function (value) {
     data = value.eventos
+    evento = data[id - 1]
+    html = `<h2>${evento.nombre}</h2>
+    <p><b>Fecha: </b>${evento.fecha}</p>
+    <p>${evento.descripcion}</p>
+    <p><b>Lugar: </b>${evento.lugar}</p>
+    <p><b>Invitados: </b>${evento.invitados}</p>
+    <p><b>Precio: ${evento.precio}</b></p>`
+    $('#evento').html(html)
   })
-
-  //Busca el elemento en el arreglo
-  var evento = data[id-1]
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
-  var html = `<h2>${evento.nombre}</h2>
-  <p><b>Fecha: </b>${evento.fecha}</p>
-  <p>${evento.descripcion}</p>
-  <p><b>Lugar: </b>${evento.lugar}</p>
-  <p><b>Invitados: </b>${evento.invitados}</p>
-  <p><b>Precio: ${evento.precio}</b></p>` 
-
-  //Modifica el DOM agregando el html generado dentro del div con id=evento
-  $('#evento').html(html)
 });
