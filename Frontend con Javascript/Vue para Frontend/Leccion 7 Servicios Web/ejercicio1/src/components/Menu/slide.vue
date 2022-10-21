4a4bea4bfced8ed34731e3d8840cbe85f6ce7078
Camilo <cacespedes@uninorte.edu.co>
2022-10-21 16:39:49 -0500

    Leccion 7 terminada

---
+++ Frontend con Javascript/Vue para Frontend/Leccion 7 Servicios Web/ejercicio1/src/components/Menu/slide.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <MenuBar v-bind="this.$attrs" @openMenu="openMenu" @closeMenu="closeMenu">
+            <slot></slot>
+        </MenuBar>
+    </div>
+</template>
+
+<script>
+    import MenuBar from '../MenuPrincipal.vue';
+    export default {
+      name: 'SlideMenu',
+      components: {
+        MenuBar
+      },
+      methods: {
+        openMenu() {
+          this.$emit('openMenu');
+        },
+        closeMenu() {
+          this.$emit('closeMenu');
+        }
+      }
+    };
+</script>
+
